ed6cadc416db7ee3c380a9d73bff39ba9deccb87
Kel <michael.buen@ienablemuch.com>
2014-09-25 22:26:04 +0800

    moved code to correct path

--- PlaySignalR/TmntHub.cs
+++ PlaySignalR/TmntHub.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 
-namespace PlaySignalR.Scripts
+namespace PlaySignalR
 {
     // Teenage Mutant Ninja Turtles Hub
     public class TmntHub : Hub
