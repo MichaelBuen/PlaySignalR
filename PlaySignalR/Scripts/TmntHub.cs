3e39d154699da57fada74861ed6a77fd4323f22b
Kel <michael.buen@ienablemuch.com>
2014-09-25 22:20:01 +0800

    comment

--- PlaySignalR/Scripts/TmntHub.cs
+++ PlaySignalR/Scripts/TmntHub.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNet.SignalR;
 
 namespace PlaySignalR.Scripts
 {
-    // Teenage Mutant Ninja Turtle Hub
+    // Teenage Mutant Ninja Turtles Hub
     public class TmntHub : Hub
     {
         public void TellAprilONeil(string name, string message)
